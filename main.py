aaf3fab7e944085f2049f2357f0d7e22afa4f093
QqWwEe123123123 <64291296+QqWwEe123123123@users.noreply.github.com>
2020-12-18 21:34:25 -0500

    adding/removing walls

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 
-from NODEClass import NODE
+from NODEClass import NODE, COLOR
 
 """
 col and row are mostly for position on display
@@ -16,26 +16,16 @@ width = 50 - panelSize
 CELLSIZE = WIDTH // height
 delay = 0
 
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-GREY = (150, 150, 150)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-TURQUOISE = (0, 183, 255)
-ORANGE = (255, 102, 0)
-YELLOW = (255, 255, 0)
-BLUE = (0, 0, 255)
-PURPLE = (170, 0, 204)
-
 
 class DISPLAY:
     def __init__(self):
+        self.walls = []
         self.grid = createGrid()
 
         self.start = self.grid[20][20]
-        self.start.setCell(1, RED, True)
+        self.start.setCell(1, COLOR["RED"], True)
         self.end = self.grid[20][21]
-        self.end.setCell(2, GREEN, True)
+        self.end.setCell(2, COLOR["GREEN"], True)
 
     def run(self):  # main loop
         run = True
@@ -44,6 +34,9 @@ class DISPLAY:
         # events
         startEndDrag = False
         startEndPos = (0, 0)
+
+        wallDrag = False
+        removeDrag = False
 
         while run:
             for event in pygame.event.get():
@@ -56,39 +49,79 @@ class DISPLAY:
                         if state == 1 or state == 2:  # current state is the start or end
                             startEndDrag = True
                             startEndPos = (mouseY, mouseX)
-                            print("clicked")
+                        elif state == 0 or state == 3:
+                            wallDrag = True
+                    if event.button == 3:
+                        mouseY, mouseX = getMousePos()
+                        current = self.grid[mouseY][mouseX]
+                        if current.state == 3 or current.state == 0:
+                            removeDrag = True
+
                 if event.type == pygame.MOUSEBUTTONUP:
                     if event.button == 1:
                         startEndDrag = False
+                        wallDrag = False
+                    if event.button == 3:
+                        removeDrag = False
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_z:
+                        self.clearALL()
 
             # update
             if startEndDrag:
                 mouseY, mouseX = getMousePos()
                 if vaild(mouseX, mouseY) and startEndPos != (mouseY, mouseX):
                     self.grid[startEndPos[0]][startEndPos[1]].moveCell(self.grid[mouseY][mouseX])
+                    self.grid[startEndPos[0]][startEndPos[1]], self.grid[mouseY][mouseX] = self.grid[mouseY][mouseX], \
+                                                                                           self.grid[startEndPos[0]][
+                                                                                               startEndPos[1]]
+
                     startEndPos = (mouseY, mouseX)
+
+            if wallDrag:
+                mouseY, mouseX = getMousePos()
+                if vaild(mouseX, mouseY):
+                    current = self.grid[mouseY][mouseX]
+                    if current.state != 1 and current.state != 2:
+                        current.setToWall()
+                        self.walls.append(current)
+
+            if removeDrag:
+                mouseY, mouseX = getMousePos()
+                if vaild(mouseX, mouseY):
+                    current = self.grid[mouseY][mouseX]
+                    if current.state == 3:
+                        current.setCell(0, -1, False)
+                        self.walls.remove(current)
             self.draw()
             pygame.display.update()
             clock.tick(120)
         pygame.display.quit()
 
     def draw(self):  # draw all graphic
-        screen.fill(GREY)  # set background color
+        screen.fill(COLOR["GREY"])  # set background color
         # draw grid
         for i in self.grid:
             for cell in i:
                 cell.draw(screen)
+
+    def clearALL(self):
+        for i in self.grid:
+            for j in i:
+                if j.state == 3:
+                    j.resetCell()
 
 
 def createGrid():
     grid = [[NODE(j, i, CELLSIZE) for i in range(width)] for j in range(height)]
     # border
     for i in range(width):
-        grid[0][i].setCell(3, BLACK, True)
-        grid[height - 1][i].setCell(3, BLACK, True)
+        grid[0][i].setCell(4, COLOR["BLACK"], True)
+        grid[height - 1][i].setCell(4, COLOR["BLACK"], True)
     for j in range(height):
-        grid[j][0].setCell(3, BLACK, True)
-        grid[j][width - 1].setCell(3, BLACK, True)
+        grid[j][0].setCell(4, COLOR["BLACK"], True)
+        grid[j][width - 1].setCell(4, COLOR["BLACK"], True)
     return grid
 
 
@@ -104,11 +137,3 @@ def vaild(x, y):
 
 if __name__ == '__main__':
     DISPLAY().run()
-
-
-"""
-fix dragging start/end to any point in grid
-add/delete walls feature
-implement A-star,bfs,dfs,ect.
-add side interface and functionally buttons
-"""
